93438ade88a5d75f7e5190f9f9ea74fc179c5bfa
wysmallz <wysmallz@163.com>
2017-12-15 18:10:28 +0800

    首页创建demo

---
+++ OctOcean.DataService/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace OctOcean.DataService
+{
+    public class Class1
+    {
+    }
+}
