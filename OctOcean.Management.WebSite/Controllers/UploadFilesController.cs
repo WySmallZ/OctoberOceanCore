5a63619d512af24993211c3a0fbbe9528682c9d7
wysmallz <wysmallz@163.com>
2018-01-11 17:59:00 +0800

    添加文件上传功能

---
+++ OctOcean.Management.WebSite/Controllers/UploadFilesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OctOcean.Management.WebSite.Controllers
+{
+    [Route("Upload")]
+    public class UploadFilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult wy(string name)
+        {
+            return Content("Post");
+        }
+    }
+}
