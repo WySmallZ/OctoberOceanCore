1a311f1aa2be47f72baad0962fe5e364bcd74d2c
wysmallz <wysmallz@163.com>
2018-01-12 18:17:45 +0800

    添加图片的批量上传功能

--- OctOcean.Management.WebSite/Controllers/UploadFilesController.cs
+++ OctOcean.Management.WebSite/Controllers/UploadFilesController.cs
@@ -2,21 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OctOcean.Management.WebSite.Models;
 
 namespace OctOcean.Management.WebSite.Controllers
 {
     [Route("Upload")]
     public class UploadFilesController : Controller
     {
-        public IActionResult Index()
+        [Route("{ArticleKey}/{UploadType?}")]
+        public IActionResult Index(string ArticleKey, string UploadType)
         {
-            return View();
+            string _extensions = string.Empty;
+            string _mimeTypes = string.Empty;
+            string _title = string.Empty;
+            switch (UploadType)
+            {
+                case "img":
+                    _extensions = "gif,jpg,jpeg,bmp,png";
+                    _mimeTypes = "image/*";
+                    _title = "Images";
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            Ex_UploadFile_M uploadmodel = new Ex_UploadFile_M()
+            {
+                Accept_Extensions = _extensions,
+                Accept_MimeTypes = _mimeTypes,
+                Accept_Title = _title,
+                Chunked = 0,
+                ArticleKey = ArticleKey
+            };
+
+
+
+
+
+
+            return View(uploadmodel);
         }
-        [HttpPost]
-        public IActionResult wy(string name)
+
+
+
+
+
+        [Route("Send/{ArticleKey}")]
+        public IActionResult SendSmallFile(string ArticleKey)
         {
+
+            List<IFormFile> files = this.HttpContext.Request.Form.Files as List<IFormFile>;
+
             return Content("Post");
+
         }
     }
 }
---
+++ OctOcean.Management.WebSite/Models/Ex_UploadFile_M.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OctOcean.Management.WebSite.Models
+{
+    public class Ex_UploadFile_M
+    {
+        public string Accept_Title { get; set; }
+        public string Accept_Extensions { get; set; }
+        public string Accept_MimeTypes { get; set; }
+        /// <summary>
+        /// 是否开启分片，0：false，1：true，之所以使用0/1是为了js方便转换为bool类型
+        /// </summary>
+        public byte Chunked { get; set; }
+        public string ArticleKey { get; set; }
+    }
+}
