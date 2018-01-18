5d8bb22ba2a7a8a97ef48fb9a039f689efa1d833
wysmallz <wysmallz@163.com>
2018-01-18 19:04:23 +0800

    添加图片缓存清理功能

--- OctOcean.Management.WebSite/Controllers/ArticleImageController.cs
+++ OctOcean.Management.WebSite/Controllers/ArticleImageController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
- 
+using OctOcean.DataService;
 using OctOcean.Management.WebSite.Models;
 using OctOcean.Utils;
 
@@ -95,13 +95,13 @@ namespace OctOcean.Management.WebSite.Controllers
             int _status = -1;
             string _msg = "";
             string _fileName = "";
-            if(requestForm!=null)
+            if (requestForm != null)
             {
                 List<IFormFile> files = requestForm.Files as List<IFormFile>;
                 if (files != null && files.Count > 0)
                 {
                     //获取旧的信息
-                    var entity= imgdal.GetPri_ArticleImage_Entity(ImgKey);
+                    var entity = imgdal.GetPri_ArticleImage_Entity(ImgKey);
                     if (entity == null)
                     {
                         _status = 4;
@@ -112,21 +112,21 @@ namespace OctOcean.Management.WebSite.Controllers
                         string imageCacheDir = Path.Combine(OctOcean.Utils.OctOceanGlobal.Config.FileRoot_Cache_Image, entity.ArticleKey);
                         if (!Directory.Exists(imageCacheDir)) { Directory.CreateDirectory(imageCacheDir); };
                         var formFile = files[0];
-                       
+
                         string imgname = ImgKey + Path.GetExtension(formFile.FileName); //获取上传图片的扩展名
                         _fileName = imgname;
- 
+
                         string fn = Path.Combine(imageCacheDir, imgname);
                         using (var stream = new FileStream(fn, FileMode.Create))
                         {
                             await formFile.CopyToAsync(stream);
                             _status = 1;
-                            _msg = Utils.OctOceanGlobal.Config.UrlRoot_Cache_Image + "/" +entity. ArticleKey + "/" + imgname;
+                            _msg = Utils.OctOceanGlobal.Config.UrlRoot_Cache_Image + "/" + entity.ArticleKey + "/" + imgname;
 
                         }
 
                     }
-                    
+
                 }
                 else
                 {
@@ -140,13 +140,13 @@ namespace OctOcean.Management.WebSite.Controllers
                 _status = 2;
                 _msg = "服务器未响应信息";
             }
-            return Json(new { status=_status, msg = _msg, fileName = _fileName });
+            return Json(new { status = _status, msg = _msg, fileName = _fileName });
 
         }
 
 
         [Route("Confirm/{ArticleKey}")]
-        public ActionResult ConfirmFile( string ArticleKey, string CacheFileNames) //确定图片时调用
+        public ActionResult ConfirmFile(string ArticleKey, string CacheFileNames) //确定图片时调用
         {
 
 
@@ -162,7 +162,7 @@ namespace OctOcean.Management.WebSite.Controllers
 
 
             string[] farr = CacheFileNames.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-          
+
 
             foreach (string fn in farr)
             {
@@ -204,19 +204,19 @@ namespace OctOcean.Management.WebSite.Controllers
 
 
         //编辑单个图片的时候
-        [Route("Edit/{ImageKey}",Name ="image_edit")]
+        [Route("Edit/{ImageKey}", Name = "image_edit")]
         public ActionResult Edit(string ImageKey)
         {
-            OctOcean.Entity.Pri_ArticleImage_Entity entity= imgdal.GetPri_ArticleImage_Entity(ImageKey);
+            OctOcean.Entity.Pri_ArticleImage_Entity entity = imgdal.GetPri_ArticleImage_Entity(ImageKey);
             if (entity == null) return Content("图片信息无效");
             return View(entity);
         }
 
         [HttpPost("Save")]
         [ValidateAntiForgeryToken]
-        public ActionResult Save([Bind("ImgKey,ImgName,Height,Width")]Entity.Pri_ArticleImage_Entity img_Entity,string NewImgSrc)
+        public ActionResult Save([Bind("ImgKey,ImgName,Height,Width")]Entity.Pri_ArticleImage_Entity img_Entity, string NewImgSrc)
         {
-            var entity= imgdal.GetPri_ArticleImage_Entity(img_Entity.ImgKey);
+            var entity = imgdal.GetPri_ArticleImage_Entity(img_Entity.ImgKey);
             if (entity == null) return Content("图片信息无效");
             //判断是否重新上传过新的文件
             if (!string.IsNullOrWhiteSpace(NewImgSrc))
@@ -230,20 +230,20 @@ namespace OctOcean.Management.WebSite.Controllers
                 entity.ImgName = img_Entity.ImgName;
                 entity.Src = OctOceanGlobal.Config.UrlRoot_Web_Image + $"/{entity.ArticleKey}/{img_Entity.ImgName}";
             }
-            
-            
-          
-         
+
+
+
+
             entity.Height = img_Entity.Height;
             entity.Width = img_Entity.Width;
             entity.UpdateTime = DateTime.Now;
             imgdal.UpdatePri_ArticleImage(entity);
-            
-             
 
-           // return Content("<script>alert('Hello World!');</script>", "application/x-javascript");
+
+
+            // return Content("<script>alert('Hello World!');</script>", "application/x-javascript");
             //return RedirectToRoute("image_edit",new { ImageKey = entity.ImgKey });
-           return RedirectToAction("Edit", new { ImageKey = entity.ImgKey });
+            return RedirectToAction("Edit", new { ImageKey = entity.ImgKey });
 
 
         }
@@ -266,15 +266,15 @@ namespace OctOcean.Management.WebSite.Controllers
             if (entity == null) return Json(new { status = 2, msg = "数据获取失败" });
             try
             {
-               i= imgdal.DeletePri_ArticleImageByImgKey(ImgKey);
+                i = imgdal.DeletePri_ArticleImageByImgKey(ImgKey);
                 _status = 1;
-                
+
             }
             catch (Exception ex)
             {
                 _status = 4;
                 _msg = ex.Message;
-                
+
             }
 
             //删除数据
@@ -304,6 +304,74 @@ namespace OctOcean.Management.WebSite.Controllers
 
             return Json(new { status = _status, msg = _msg });
         }
+
+        [HttpPost("Clear")]
+        public ActionResult RunClearImage()
+        {
+            int _status = -1;
+            string _msg = "";
+
+            try
+            {
+
+
+                //获取系统所有图片
+                var db_sys_all_iamges = new Pri_ArticleImage_Dal().GetAllPri_ArticleImage();
+                string cp_web_root = OctOceanGlobal.Config.FileRoot_Web_Image;
+
+                if (Directory.Exists(cp_web_root))
+                {
+                    //获取所有的articlekey所在目录
+                    string[] cp_all_web_article_diries = Directory.GetDirectories(cp_web_root);
+
+                    foreach (string cp_articlekey_dir in cp_all_web_article_diries)
+                    {
+                        string cp_articlekey = Path.GetFileNameWithoutExtension(cp_articlekey_dir + ".wy");//获取ArticleKey的值
+                        var _db_sys_article_images_where = db_sys_all_iamges.Where(a => a.ArticleKey == cp_articlekey);                                                                //判断该目录是否正在使用当中
+                        var db_sys_article_images = _db_sys_article_images_where == null ? null : _db_sys_article_images_where.Select(b => b.ImgName).ToArray();
+
+                        //如果在数据库中没有找到当前目录正在使用当中，就删除改目录
+                        if (db_sys_article_images == null || db_sys_article_images.Length == 0)
+                        {
+                            Directory.Delete(cp_articlekey_dir, true);
+                        }
+                        else
+                        {
+                            //获取当前key下的所有图片
+                            string[] cp_all_web_article_images = Directory.GetFiles(cp_articlekey_dir);
+                            foreach (string cp_img in cp_all_web_article_images)
+                            {
+                                //如果系统中没有使用该图片，就删除掉
+                                if (!db_sys_article_images.Contains(Path.GetFileName(cp_img)))
+                                {
+                                    System.IO.File.Delete(cp_img);
+                                }
+                            }
+
+                        }
+
+
+                    }
+                }
+
+                //清理缓存
+                if (Directory.Exists(OctOceanGlobal.Config.FileRoot_Cache_Image))
+                {
+                    Directory.Delete(OctOceanGlobal.Config.FileRoot_Cache_Image, true);
+
+                }
+                _status = 1;
+
+            }
+            catch (Exception ex)
+            {
+
+                _status = 4;
+                _msg = ex.Message;
+            }
+            return Json(new { status = _status, msg = _msg });
+        }
+
 
         //public IActionResult Index()
         //{
---
+++ OctOcean.Management.WebSite/Pages/ImageMaintenance/ImageClearUp.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OctOcean.DataService;
+using OctOcean.Utils;
+using System.IO;
+using OctOcean.Management.WebSite.Models;
+
+namespace OctOcean.Management.WebSite.Pages.ImageMaintenance
+{
+    public class ImageClearUpModel : PageModelBase
+    {
+
+
+       public Dictionary<string, string[]> dicClearImages = new Dictionary<string, string[]>();
+        public string UrlRoot_Cache_Image { get; private set; }= OctOceanGlobal.Config.UrlRoot_Cache_Image;
+
+        public bool BtnDisabled { get; set; }
+
+        public IActionResult OnGet()
+        {
+
+            if (!base.CheckLogin())
+            {
+                return Redirect("/login/index");
+            }
+
+
+            //获取系统所有图片
+            var sys_all_iamges = new Pri_ArticleImage_Dal().GetAllPri_ArticleImage();
+
+         
+ 
+            string web_root = OctOceanGlobal.Config.FileRoot_Web_Image;
+            if (Directory.Exists(web_root))
+            {
+                //获取所有的文章目录
+                string[] all_web_article_diries = Directory.GetDirectories(web_root);
+                foreach (string articlekey_dir in all_web_article_diries)
+                {
+                    string articlekey = Path.GetFileNameWithoutExtension(articlekey_dir+".wy");//获取ArticleKey的值
+
+
+
+                    //获取当前key下的所有图片
+                    string[] all_web_article_images = Directory.GetFiles(articlekey_dir);
+                    //如果图片中存在一张没在使用的情况
+                    dicClearImages[articlekey]= all_web_article_images.Select(c => Path.GetFileName(c))
+                        .Where(a => sys_all_iamges.FirstOrDefault(b => b.ArticleKey == articlekey && b.ImgName == a) == null).ToArray();
+ 
+                }
+            }
+
+            BtnDisabled= dicClearImages.Values.Sum(a => a.Length) == 0;
+
+            return Page();
+
+        }
+         
+    }
+}
